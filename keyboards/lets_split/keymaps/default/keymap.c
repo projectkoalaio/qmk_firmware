8ca0f0fd9e9ee74f84e0a6fbfcd41dba26e99caa
rbeaty <rob@myocv.com>
2017-03-20 15:45:48 -0500

    moved shift to hold space

--- keyboards/lets_split/keymaps/default/keymap.c
+++ keyboards/lets_split/keymaps/default/keymap.c
@@ -45,10 +45,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
-  KC_Q,               KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-  KC_A,               KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_BSPC, \
-  MT(MOD_LSFT, KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-                               KC_LCTL, LOWER,   KC_ENT,  KC_SPC,  RAISE,   TG(16)  \
+  KC_Q, KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
+  KC_A, KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_BSPC, \
+  KC_Z, KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
+                 KC_LCTL, LOWER,   KC_ENT, MT(MOD_LSFT, KC_SPC), RAISE,   TG(16)  \
 ),
 
 /* Colemak
@@ -245,8 +245,8 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) //
     case 5: // this would trigger when you hit a key mapped as M(5)
       if (record->event.pressed) {
         return MACRO( D(LCTL), D(PGDOWN), U(PGDOWN), U(LCTL), END  ); // control page down
-      } 
-      break;
+      }
+    break;
   }
   return MACRO_NONE;
 };
